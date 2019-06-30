77a393c9302018fa73da5f82e13d765b3627f841
shadow-horse <yuanjunhu30@gmail.com>
2019-07-01 00:02:11 +0800

    python爬虫 爬起静态页面href

---
+++ SpiderScanner/README.md
@@ -0,0 +1,17 @@
+## 基于Python的爬虫扫描器
+
+### 1. 选定爬虫的环境 
+
+https://weibo.com/  
+
+### 2. 环境配置
+
+1. python3.0  Mac  
+2. pip isntall selenium 模拟浏览器  
+3. pip install beautifulsoup4
+4. pip install lxml
+5. pip install selenium 
+6. pip install requests
+
+
+
---
+++ SpiderScanner/spider/HrefSpider.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+"""爬虫：爬取Href属性的链接"""
+import requests
+from bs4 import BeautifulSoup
+from _socket import timeout
+class HrefSpider:
+    '''
+    初始化参数
+    '''
+    def setUrl(self,url):
+        self.url = url;
+        self.list = []
+    '''
+    此函数用于获取网页的内容
+    '''
+    def getHtmlText(self,url):
+        try:
+            res = requests.get(url,timeout = 6)
+            #判断是否HttpError
+            res.raise_for_status()
+            res.encoding = res.apparent_encoding
+            return res.text
+        except:
+            print("getHtmlText 产生异常")
+    '''
+    此函数用于解析获取href属性值
+    '''    
+    def parseHtmlText(self,text):
+        #解析html代码
+        soup = BeautifulSoup(text,'html.parser')
+        #模糊搜索获取HTML代码所有<a href="">属性值
+        a_hrefs = soup.find_all('a', attrs={'href':True})
+        return a_hrefs
+    
+    def getHrefs(self):
+        text = self.getHtmlText(self.url)
+        a_hrefs = self.parseHtmlText(text)
+        
+        for a in a_hrefs:
+            self.list.append(a.get('href'))
+        return self.list
+    
+if __name__ == "__main__":
+    spider = HrefSpider();
+    spider.setUrl("https://www.cnblogs.com/huwt")
+    result = spider.getHrefs()
+    for a in result:
+        print(a)
+    print("spider end.")
+
