3906c5d74cf3a4bb0ed022260c865c8f21d1f6b8
shadow-horse <yuanjunhu30@gmail.com>
2019-07-01 08:44:59 +0800

    等待AJAX异步加载

--- SpiderScanner/spider/DynamicSpider.py
+++ SpiderScanner/spider/DynamicSpider.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import time
 from selenium import webdriver
 
 """
@@ -8,16 +9,18 @@ from selenium import webdriver
 class DynamicSpider:
     def __init__(self):
         self.browser = webdriver.PhantomJS("/Users/snow/programs/phantomjs-2.1.1-macosx/bin/phantomjs")
-        
+        #explicit waits 和implicit wait 在获取element前，等待Ajax执行完毕
+        self.browser.implicitly_wait(10)
     '''
     初始化
     '''
     def setUrl(self,url):
         self.url = url
-    
+        
     '''
     获取异步请求加载完成后的页面
     '''
     def getAsynHtmltext(self):
-        self.browser.get(self.url)
+        self.browser.get(self.url)  
+        #time.sleep(10) 直接设置等待10s
         return self.browser.page_source
