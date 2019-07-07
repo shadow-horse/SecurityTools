4301b81e7d489648bbd8b5c4d308f4eef7be3452
shadow-horse <yuanjunhu30@gmail.com>
2019-07-07 14:22:39 +0800

    文件操作和POC示例

---
+++ SpiderScanner/tools/FileOperation.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+import json
+
+#文件异常未处理
+class FileOperaton:
+    
+    #保存爬取的链接，暂时不考虑写过程的去重
+    def fileWrite(self,filename="spider.urls.txt",data):
+        fo = open(filename,"w+")
+        for line in data.keys():
+            fo.write(line +":"+json.dumps(data[line])+"\n")
+            fo.close()
+    
+    #读取爬虫文件，返回链接对象
+    def fileRead(self,filename="spider.urls.txt"):
+        fo = open(filename,'r')
+        urllists = {}
+        for line in fo.readlines():
+            index = line.index(":")
+            key = line[:index]
+            value = line[index:]
+            urllists[key]=value
+        return urllists
+    
+    #读取payload文件，返回poc
+    '''
+    {"poc":"<script>console.log('12345678');</script>","type":"console","verify":"12345678"}
+    poc: 发送的攻击payload
+    type: 检测响应的方式
+    verify: 检测的字符串或关键代码
+    '''
+    def loadPayloads(self,filename="payloads.txt"):
+        fo = open(filename)
+        payloads = []
+        for line in fo.readlines():
+            payloads.append(line)
+            print(payloads)
+            return payloads
