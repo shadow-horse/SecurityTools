cd78fa7c908326670efdaecd5985bff563feacdb
shadow-horse <yuanjunhu30@gmail.com>
2019-07-09 12:22:08 +0800

    启动代码

--- SpiderScanner/start.py
+++ SpiderScanner/start.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+import sys
 from spider import StaticSpider
 from spider import DynamicSpider
 from scanner import DomxssScanner
@@ -7,6 +7,7 @@ from tools import Deldump
 from tools import FileOperation
 from spider import SpiderOperation
 from test.test_decimal import file
+from scanner import ScanOperation
 
 def test1():    
     print("#####################################")
@@ -42,22 +43,20 @@ def test1():
     print("#############END##################")
     
 
-def test2():
-    url = "http://127.0.0.1:8080/web/domxss"
+def spiderURL(url,deep):
     spider = SpiderOperation.SpiderOperation()
-    spider.init(1, url)
+    spider.init(deep, url)
     spider.startSpider()
     rurls = spider.getUrls()
-    print("spider results:")
-    for u in rurls.keys():
-        print("%s %s" % (u,rurls[u]))
     fileop = FileOperation.FileOperaton()
     fileop.fileWrite(rurls)
     
+    print("===================================")
+    print("共爬取到URL如下：")
     fr = fileop.fileRead()
     for u in fr.keys():
         print("%s %s" % (u,fr[u]))
-    print(("##############END#################"))
+    print(("==================================="))
     
 def test3():
     
@@ -70,21 +69,40 @@ def test3():
         dscanner.__init__()
         dscanner.setPayloads(payloads)
         dscanner.dealUrl(urls[url])
+        dscanner.end()
+        
     
+ 
+def scannerDomxss():
+    scanner = ScanOperation.ScanOperation();
+    scanner.startScan()
+    scanner.getScanresults()
     
+def scanner(url,deep):
+    spiderURL(url, deep)
+    scannerDomxss()
+'''
+环境配置：
+    1. python3.0   
+    2. pip isntall selenium   模拟浏览器  
+    3. pip install beautifulsoup4   安装HTML解析包
+    4. pip install lxml
+    5. 安装phantomjs:  需要修改DomxssScanner.py和DynamicSpider.py脚本中的配置，替换为当前环境的phantomjs路径
+        webdriver.PhantomJS("/Users/snow/programs/phantomjs-2.1.1-macosx/bin/phantomjs",service_args=self.service_args)  
+    6. pip install requests   安装HTTP包
+    
+'''
+'''
+说明：
+    参数1:    url 待爬取的目标URL
+    参数2:    deep 爬取的深度
+    运行示例:    python start.py http:www.baidu.com 2
+'''       
 if __name__ == "__main__":
-#     test2()
-    test3()
+    url = "http://47.104.218.243:8080/web/"
+    deep = 2
+    if len(sys.argv) >= 3:
+        url = sys.argv[1]
+        deep = sys.argv[2]
     
-#     domscanner = DomxssScanner.DomxssScanner()
-#     payloads = []
-#     
-#     a = "{\"poc\":\"<script>alert(0);</script>\",\"check\":\"console\"}"
-#     b = "{\"poc\":\"<script>alert(0);</script>\",\"check\":\"tag\"}"
-#     payloads.append(a)
-#     payloads.append(b)
-#     url = {'id': 2, 'method': 'GET', 'url': 'http://127.0.0.1:8009/webtest/?default=hello&abc=a', 'postdata': ''}
-#     domscanner.setUrl("http://127.0.0.1:8009/webtest/?default=%3Cscript%3Econsole.log(1234);%3C/script%3E")
-#     domscanner.setPayloads(payloads)
-#     domscanner.dealUrl(url)
-#     domscanner.scanUrl(payloads)
+    scanner(url,deep)
