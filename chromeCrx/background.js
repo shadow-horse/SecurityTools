e12011d70fe7e902ffca3b9bb0cc553bbac0333c
shadow-horse <yuanjunhu30@gmail.com>
2021-07-26 22:38:35 +0800

    chrome插件流量转发

--- chromeCrx/background.js
+++ chromeCrx/background.js
@@ -1,9 +1,195 @@
+//上报请求
+function sendReqserver(reqid,contens){
+    var xhr = new XMLHttpRequest();
+    console.log(reqid);
+    //防止循环监听，过略上报的域名
+    xhr.open('GET', 'http://47.104.218.243/ssrf/ssrf.php?rand='+reqid, true);
+
+    xhr.onload = function () {
+       // 请求结束后,在此处写处理代码
+       console.log('send success~~')
+    };
+
+    xhr.send(null);
+
+}
+// 监听函数, 当符合代理配置的, 重写请求路径
+function getRequests (details) {
+//  console.log(details);
+  console.log("===========================================================");
+  console.log("reqid:  " + details['requestId']);
+  console.log("url:    " + details['url']);
+  console.log("method: " + details['method']);
+  if(details['method'] == 'POST'){
+    console.log(details['requestBody']);
+    let requestBody = details['requestBody'];
+    if(details['type'] == 'main_frame'){
+        console.log("reqbody:  ");
+        console.log(requestBody['formData']);
+    }else if(details['type'] == 'xmlhttprequest'){
+        console.log("reqbody:  ");
+        console.log(requestBody['raw']);
+    }
+  }else
+  {
+    console.log('method get！')
+  }
+  return {cancel: false}
+}
+function getheaders(details){
+//    console.log(details);
+    console.log("********************************");
+    console.log("reqid: " + details['requestId']);
+    console.log("requestHeaders: ")
+    console.log(details['requestHeaders'])
+
+//    sendReqserver(details['requestId'],"");
+}
+function del_callback (parameters) { /* ... */ }
+
+// 添加代理监听方法
+function addRequestlistener () {
+  console.log('addRequestlistener=====================');
+  urls = assmdomain.split(',');
+  all_urls = "";
+  for (i = 0; i<urls.length;i++){
+     urls[i] = "*://" + urls[i] +"/*";
+     console.log(urls[i]);
+  }
+
+  chrome.webRequest.onBeforeRequest.addListener(
+    getRequests,
+    {urls: urls},
+    ["extraHeaders","requestBody"]
+  )
+  // alert(chrome.webRequest.onBeforeRequest.hasListeners())
+}
+// 移除代理监听
+function delRequestlistener () {
+    console.log('delRequestlistener=====================');
+      urls = assmdomain.split(',');
+      all_urls = "";
+      for (i = 0; i<urls.length;i++){
+         urls[i] = "*://" + urls[i] +"/*";
+         console.log(urls[i]);
+      }
+
+  chrome.webRequest.onBeforeRequest.removeListener(
+    getRequests,
+    {urls: urls},
+    ["extraHeaders","requestBody"]
+  )
+  // alert(chrome.webRequest.onBeforeRequest.hasListeners())
+}
+
+//添加headers监听
+function addHeaderlistener(){
+    console.log('addHeaderlistener=====================');
+    urls = assmdomain.split(',');
+    all_urls = "";
+    for (i = 0; i<urls.length;i++){
+         urls[i] = "*://" + urls[i] +"/*";
+         console.log(urls[i]);
+    }
+    chrome.webRequest.onBeforeSendHeaders.addListener(
+        getheaders,
+        {urls:urls},['extraHeaders','requestHeaders']);
+}
+function delHeaderlistener(){
+    console.log('delHeaderlistener=====================');
+        urls = assmdomain.split(',');
+        all_urls = "";
+        for (i = 0; i<urls.length;i++){
+             urls[i] = "*://" + urls[i] +"/*";
+             console.log(urls[i]);
+        }
+
+    chrome.webRequest.onBeforeSendHeaders.removeListener(
+        getheaders,
+        {urls: urls},['extraHeaders','requestHeaders']);
+}
+
+
 // 注册回调，当收到请求的时候触发
 chrome.extension.onRequest.addListener(({ tabId, args }) => {
+  console.log('requrst listener');
 
-    console.log("hello world");
   // 在给定tabId的tab页中执行脚本
   chrome.tabs.executeScript(tabId, {
     code: `console.log(...${JSON.stringify(args)});`,
   });
+
+  //发起请求
 });
+
+
+var assmname = "";
+var assmdomain = "";
+var assmswitch = false;
+
+function getassminfo(){
+    chrome.storage.local.get('assmname',function(result){
+            assmname = result['assmname'];
+        });
+        chrome.storage.local.get('assmdomain',function(result){
+               assmdomain = result['assmdomain'];
+        });
+        chrome.storage.local.get('assmswitch',function(result){
+               assmswitch = result['assmswitch'];
+         });
+}
+
+
+
+
+//初始化,设置监听的启停
+function initassmswitch(){
+    chrome.storage.local.get('assmswitch',function switchstatus(result){
+                                              value = result['assmswitch'];
+                                              if(value){
+                                                  console.log('add listener.......');
+                                                  addRequestlistener();
+                                                  addHeaderlistener();
+                                              }else{
+                                                      console.log('close listener.......');
+                                                      delRequestlistener();
+                                                      delHeaderlistener();
+                                                   }
+                                          });
+}
+getassminfo();
+setTimeout(initassmswitch,1000);
+
+function testassminfo(){
+    console.log('assmname: ' + assmname);
+    console.log('assmdomain: ' + assmdomain);
+}
+
+setTimeout(testassminfo,100);
+
+
+
+function updateListener()
+{
+    //清理
+    delHeaderlistener();
+    delRequestlistener();
+
+    console.log('assm switch is ' + assmswitch);
+    if(assmswitch){
+        console.log('add listener.......');
+        addHeaderlistener();
+        addRequestlistener();
+    }else{
+        console.log('close listener.......');
+
+    }
+}
+
+//增加存储监听按钮，修改配置信息
+chrome.storage.onChanged.addListener((changes, namespace) => {
+    //如果配置信息修改，则重新设置监听器
+    getassminfo();
+    setTimeout(updateListener,1000);
+
+})
--- chromeCrx/buttonclick.js
+++ chromeCrx/buttonclick.js
@@ -1,41 +1,70 @@
-var assmname = localStorage.assmname;
-var assmdomain = localStorage.assmdomain;
-var assmbutton = localStorage.assmbutton;
-var assmscan = localStorage.assmscan;
+var assmname = "";
+var assmdomain = "";
+var assmswitch = false;
 
-console.log(assmname);
-assmbutton = assmbutton?assmbutton:'false';
+//函数操作定义
 
+$(document).ready(function(){
+            $("#onoffswitch").click(function(){
+                clickSwitch();
+            });
 
-if(assmbutton === 'true')
-{
-    console.log('====');
-    value = document.getElementsById("onoffswitch");
-    console.log(value);
+            $("#savebutton").click(function(){
+                saveassminfo();
+            });
 
-    $("#onoffswitch").attr("checked",true);
-}else
-{
-    $("#onoffswitch").attr("checked",false);
-}
+            $("#getbutton").click(function(){
+                getassminfo();
+            });
+        });
 
-if(assmname !=""){
-    $("#assmname").val(localStorage.assmname);
-    $("#assmdomain").val(localStorage.assmdomain);
-}
-
-function setassmSwitch(onoff){
-    chrome.storage.local.set({'assmswitch': onoff}, function() {
-            console.log("store switch is " + onoff);
+function setassmSwitch(status){
+    chrome.storage.local.set({assmswitch: status}, function() {
+               console.log("store switch is " + status);
             });
 }
+function getassmSwitch(){
+    console.log("get assm switch ");
+    chrome.storage.local.get('assmswitch',function(result){
+        console.log(result['assmswitch']);
+        return result['assmswitch'];
+    }
+    );
+}
+//数据初始化
+function initassmSwitch(ob){
+    console.log("init assm switch status.");
+    chrome.storage.local.get('assmswitch',function(result){
+        value = result['assmswitch'];
+        document.getElementById("onoffswitch").checked = value;
 
-function onClickHander(obj){
-            console.log("click off/on  ....");
-            onoff = document.getElementsByName("onoffswitch")[0].checked;
-            console.log(onoff);
-			console.log("在ON的状态下");
-			setassmSwitch
+    }
+    );
+}
+
+function clickSwitch(){
+    console.log('click switch');
+    checked = document.getElementById("onoffswitch").checked;
+    setassmSwitch(checked);
+}
+
+function setassminfo(name,domain){
+    chrome.storage.local.set({assmname:name},function(){
+        console.log("store assm name: " + name);
+    });
+    chrome.storage.local.set({assmdomain:domain},function(){
+            console.log("store assm domain: " + domain);
+    });
+
+}
+
+function initassminfo(){
+    chrome.storage.local.get('assmname',function(result){
+        $("#assmname").val(result['assmname']);
+    });
+    chrome.storage.local.get('assmdomain',function(result){
+            $("#assmdomain").val(result['assmdomain']);
+    });
 }
 
 function saveassminfo(){
@@ -45,15 +74,22 @@ function saveassminfo(){
     if(name && domain){
         name = name.replace(/(^\s*)|(\s*$)/g, '');
         domain = domain.replace(/(^\s*)|(\s*$)/g, '');
-
         if(name != '' && domain != '') {
-            localStorage.assmname = name;
-            localStorage.assmdomain = domain;
+            setassminfo(name,domain);
             alert("保存成功！");
             return;
         }
     }
     alert("保存失败，请输入有效内容！");
+}
+
+function getassminfo(){
+    chrome.storage.local.get('assmswitch',function(status){
+                console.log(status['assmswitch']);
+            });
 
 }
 
+//初始化
+initassmSwitch();
+initassminfo();
